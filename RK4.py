5ba4bfb659cc09355812ba3909b4c887760376a5
Alvaro Gonzalez <alvaroglezvilla@gmail.com>
2021-07-04 10:18:01 +0200

    arreglo

--- RK4.py
+++ RK4.py
@@ -1,77 +1,38 @@
 
 #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
 # Amo a programar un runge kutta #
-import numpy as np
-import matplotlib.pyplot as plt
-from Numeric import *
-import sys
 import math
+import matplotlib.pyplot as plt
 
-def f(x,y):
-    return x+y     # Esto devuelve la funcion que pongamos
+#functions 
+dy = lambda x,y: math.sin(x)**2*y
+f = lambda x: 2*math.exp(0,5*(x-math.sin(x)*math.cos(x))) #solucion exacta
 
-def RK4(x0,y0,xn,n):
+#Parameters
+xi=0; xf =5; h=0,5
+n = int((xf-xi)/h)
+x = 0; y = 2
 
-    #Tamaño paso#
-        h = (xn-x0)/n  
-        print('\n--------SOLUTION--------')
-        print('------------------------')    
-        print('x0\ty0\tyn')
-        print('-------------------------')
-        for i in range(n):
-            k1 = h * (f(x0, y0))
-            k2 = h * (f((x0+h/2), (y0+k1/2)))
-            k3 = h * (f((x0+h/2), (y0+k2/2)))
-            k4 = h * (f((x0+h), (y0+k3)))
-            k = (k1+2*k2+2*k3+k4)/6
-            yn = y0 + k
-            print('%.4f\t%.4f\t%.4f'% (x0,y0,yn) )
-            print('-------------------------')
-            y0 = yn
-            x0 = x0+h
-        
-        print('\nAt x=%.4f, y=%.4f' %(xn,yn))
+#visualization 
+print('x  \t\t y \t\t f(x)'); print('%f  \t  %f  \t  %f'% (x,y,f(x)))
+x_plot = []; y_RK3 = []; y_analytical = []
 
-def plot_diagram():
-	global x
-	t = 0.0
-	h = 0.1
-	for value in arange(0.1,5.0,0.1):
-		l = calculate_rungekutta_param(x,t,h)
-		x = x + l
-		x_value.append(x)
-		t = t + h
-		t_value.append(t)
-		
+#rk3 method
+
+for i in range(1,n+1):
+	x_plot.append(x); y_RK3.append(y); y_analytical.append(f(x))
+
+	k1= dy(x,y)
+	k2 = dy(x+h/2,y+k1*h/2)
+	k3 = dy(x+h/2,y-k1*h+2*k2*h)
 	
-	t = np.arange(0.0, 5.0, 0.1)
-	x_derivatives = np.arange(-15.0, 30.0, 1.0)
-	plt.figure(1)
-	plt.subplot(111)
-	plt.xlabel('time (t) ')
-	plt.ylabel('displacement (x) ')
-	plt.title('Graph Between X and t')
-	plt.plot(t_value,x_value)
-	
-	plt.figure(2)
-	plt.subplot(211)
-	plt.xlabel('value of x (x)')
-	plt.ylabel('value of x_das (y) ')
-	plt.title('Graph Between X derivatives and x')
-	plt.plot(x_derivatives,f(t0,x_derivatives))
-	plt.axis([-25.0, 25.0,-600.0,0.0])
-	plt.show()
+	y = y+(k1+4*k2+k3)*h/6  #nueva estimacion de y
 
-# Inputs
-print('Enter initial conditions:')
-x0 = float(input('x0 = '))
-y0 = float(input('y0 = '))
+	x = x+h   #cambio de punto
 
-print('Enter calculation point: ')
-xn = float(input('xn = '))
+	print('%f  \t  %f  \t  %f'% (x,y,f(x)))
 
-print('Enter number of steps:')
-step = int(input('Number of steps = '))
-
-# RK4 method call
-RK4(x0,y0,xn,step)
+x_plot.append(x); y_RK3.append(y); y_analytical.append(f(x))
+plt.plot(x_plot;y_RK3,'ro',x_plot,y_analytical)
+plt.xlabel('x'); plt.ylabel('y')
+plt.legend('RK3','Analitico')
